b04ca5e0a0a8d6b70a7d3207bb164b2b05934371
474248257@qq.com <4742428257@qq.com>
2016-03-05 21:12:45 +0800

    第5天完成

--- CarouselEffect/index.ios.js
+++ CarouselEffect/index.ios.js
@@ -8,45 +8,113 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  ListView,
+  Dimensions
 } from 'react-native';
+import VisualView from './VisualView'
+
+const { width,height } = Dimensions.get('window')
 
 class CarouselEffect extends Component {
+    
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+    };
+  }
+  
+  componentDidMount() {
+   
+    this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(data),
+    });
+  }
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Image source={require('image!blue')} style={styles.container} rseizeMode='stretch'>
+        <VisualView visualType="light" style={styles.container}>
+            <View style={styles.listView}>
+                <View style={styles.mainView}>
+                    <ListView
+                        style={{paddingRight: 20}}
+                        dataSource={this.state.dataSource}
+                        renderRow={this._pageView}
+                        horizontal={true}
+                        showsVerticalScrollIndicator={true}
+                    /> 
+                </View>
+            </View>
+        </VisualView>
+    </Image>
     );
   }
+  
+  _pageView(interest) {
+      return(
+         <View style={[styles.cell,]}>
+            <Image source={interest.image} style={styles.image} resizeMode='cover'>
+                <VisualView visualType="extraLight" style={styles.textView}>
+                    <Text style={styles.text}>{interest.text}</Text>
+                </VisualView>
+            </Image>
+         </View>
+      );
+  }
 }
+
+const data = [
+  {image: require('./images/hello.jpg'), description: "We love backpack and adventures! We walked to Antartica yesterday, and camped with some cute pinguines, and talked about this wonderful app idea. 🐧⛺️✨",text: 'Hello there, i miss u.'},
+  {image: require('./images/dudu.jpg'), description: "We love backpack and adventures! We walked to Antartica yesterday, and camped with some cute pinguines, and talked about this wonderful app idea. 🐧⛺️✨",text: '🐳🐳🐳🐳🐳'},
+  {image: require('./images/bodyline.png'), description: "We love backpack and adventures! We walked to Antartica yesterday, and camped with some cute pinguines, and talked about this wonderful app idea. 🐧⛺️✨",text: 'Training like this, #bodyline'},
+  {image: require('./images/wave.jpg'), description: "We love backpack and adventures! We walked to Antartica yesterday, and camped with some cute pinguines, and talked about this wonderful app idea. 🐧⛺️✨",text: 'I\'m hungry, indeed.'},
+  {image: require('./images/darkvarder.png'), description: "We love backpack and adventures! We walked to Antartica yesterday, and camped with some cute pinguines, and talked about this wonderful app idea. 🐧⛺️✨",text: 'Dark Varder, #emoji'},
+  {image: require('./images/hhhhh.jpg'), description: "We love backpack and adventures! We walked to Antartica yesterday, and camped with some cute pinguines, and talked about this wonderful app idea. 🐧⛺️✨",text: 'I have no idea, bitch'}
+]
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  listView: {
+      flex: 1,
   },
-  instructions: {
-    textAlign: 'center',
+  cell: {
+    marginLeft: 20,
+    width: 300,
+    height: 400,
+    borderRadius: 7,
+    overflow: 'hidden'
+  },
+  mainView: {
+      width,
+      marginTop: (95/500)*height,
+      height: 450
+  },
+  image: {
+    width: 300,
+    height: 400
+  },
+  textView: {
+    position: 'absolute',
+    overflow: 'hidden',
+    paddingLeft: 15,
+    bottom: 0,
+    width: 300,
+    height: 60,
+  },
+  text: {
+    fontSize: 16,
     color: '#333333',
-    marginBottom: 5,
-  },
+    lineHeight: 40,
+    fontFamily: 'Avenir Next'
+  }
+  
 });
 
 AppRegistry.registerComponent('CarouselEffect', () => CarouselEffect);
