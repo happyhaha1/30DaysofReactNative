8662ea3ee7da5537c60dfd10c0ff43208a422f19
童lian话 <474248257@qq.com>
2016-03-06 23:32:26 +0800

    第6天完成

---
+++ FindMyLocation/ios/RCTVisualViewManager.h
@@ -0,0 +1,13 @@
+//
+//  RCTVisualViewManager.h
+//  VisualEffectView
+//
+//  Created by happy on 16/3/4.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import "RCTViewManager.h"
+
+@interface RCTVisualViewManager : RCTViewManager
+
+@end
